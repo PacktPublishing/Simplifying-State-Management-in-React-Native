e6c85379f433fba808aad4769a5ce99a797a0db9
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2022-07-08 15:55:10 +0200

    added chapter-5 files

---
+++ chapter-5/src/components/ListOfAvatars.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, FlatList, Pressable, Image } from "react-native";
+import { ListHeaderComponent } from "./ListHeaderComponent";
+import { UserListContext } from "../context";
+
+export const ListOfAvatars = ({ navigation }) => {
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable
+        onPress={() => navigation.navigate("AvatarModal", { user: item })}
+      >
+        <Image
+          style={{ height: 56, width: 56, borderRadius: 28, marginRight: 30 }}
+          source={{
+            uri: item.url,
+          }}
+        />
+      </Pressable>
+    );
+  };
+  return (
+    <UserListContext.Consumer>
+      {({ userList }) => (
+        <View
+          style={{
+            zIndex: 100,
+            paddingVertical: 30,
+            paddingLeft: 20,
+            backgroundColor: "rgba(255,255,255, 0.85)",
+          }}
+        >
+          <FlatList
+            data={userList}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            horizontal
+            ListHeaderComponent={<ListHeaderComponent />}
+            showsHorizontalScrollIndicator={false}
+            snapToInterval={86}
+            decelerationRate='fast'
+          />
+        </View>
+      )}
+    </UserListContext.Consumer>
+  );
+};
