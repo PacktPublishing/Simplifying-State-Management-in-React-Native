75ee31e351ad14b5259eff766ab3cd9941249806
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2022-10-12 00:39:12 +0200

    chapter-8

--- chapter-8/atoms/fetchImagesAtom.js
+++
@@ -1,7 +0,0 @@
-import { requestBase } from "./utils/constants";
-
-const urlAtom = atom(requestBase + "/john_doe/likedImages.json");
-export const fetchImagesAtom = atom(async (get) => {
-  const response = await fetch(get(urlAtom));
-  return await response.json();
-});
