5ae38ad419a618f70dc6dfbc7166c695f412c3ff
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2023-01-13 22:06:16 +0100

    fixed minor avatar issues

--- chapter-4/src/components/ConversationItem.js
+++ chapter-4/src/components/ConversationItem.js
@@ -10,8 +10,6 @@ export const ConversationItem = ({ navigation, item }) => {
       name: currentUser[0].name,
       avatar: currentUser[0].url,
     });
-
-    console.log("hi, messed smth here", currentUser);
   };
   return (
     <ConversationContext.Consumer>
